a26093e40f0042e6692ac16231d16ace0ba268a9
Varis Darasirikul <varis.dara@gmail.com>
2018-06-27 00:24:44 +0800

    fix broken code

--- src/app/06-services/todos.component.ts
+++ src/app/06-services/todos.component.ts
@@ -1,25 +1,30 @@
-
 import { TodoService } from './todo.service'
+import { OnInit } from '@angular/core';
 
-export class TodosComponent { 
-  todos: any[] = [];
-  message; 
+export class TodosComponent implements OnInit{
+
+  todos: any = [];
+  message: any;
 
   constructor(private service: TodoService) {}
 
-  ngOnInit() { 
+  ngOnInit() {
     this.service.getTodos().subscribe(t => this.todos = t);
   }
 
-  add() { 
-    var newTodo = { title: '... ' };
+  add() {
+    const newTodo = {
+      title: '...'
+    };
+
     this.service.add(newTodo).subscribe(
       t => this.todos.push(t),
       err => this.message = err);
   }
 
   delete(id) {
-    if (confirm('Are you sure?'))
+    if (confirm('Are you sure?')) {
       this.service.delete(id).subscribe();
-  }  
+    }
+  }
 }
